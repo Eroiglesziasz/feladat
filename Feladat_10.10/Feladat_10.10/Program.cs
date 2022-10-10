2ea056e7e1f7f75f0382f2c0bf4036d81c9dcb94
Eroice <39998814+Eroiglesziasz@users.noreply.github.com>
2022-10-10 09:10:20 +0200

    Add files via upload

---
+++ Feladat_10.10/Feladat_10.10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Vasmegye
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. feladat");
+
+        }
+    }
+}
